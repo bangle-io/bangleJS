55b2aa43d7c33de50ed76ac75a89d1e9453410bf
Kushan Joshi <0o3ko0@gmail.com>
2025-01-05 01:14:07 -0500

    Refactor exports in prosemirror-all index.ts to enhance module structure
    - Updated index.ts to export schemaBasicNodes and schemaBasicMarks from prosemirror-schema-basic.
    - Added PMPlugin and PMSelection exports from prosemirror-state.
    - Included PMNode export from prosemirror-model for improved accessibility of ProseMirror components.

--- packages/prosemirror-all/src/index.ts
+++ packages/prosemirror-all/src/index.ts
@@ -6,8 +6,14 @@ export * from 'prosemirror-history';
 export * from 'prosemirror-inputrules';
 export * from 'prosemirror-keymap';
 export * from 'prosemirror-model';
-export * from 'prosemirror-schema-basic';
 export * from 'prosemirror-state';
 export * from 'prosemirror-transform';
 export * from 'prosemirror-view';
+export {
+  nodes as schemaBasicNodes,
+  marks as schemaBasicMarks,
+} from 'prosemirror-schema-basic';
+export { Plugin as PMPlugin } from 'prosemirror-state';
+export { Selection as PMSelection } from 'prosemirror-state';
+export { Node as PMNode } from 'prosemirror-model';
 export * as PMTestBuilder from 'prosemirror-test-builder';
